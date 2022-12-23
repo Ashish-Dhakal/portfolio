71784a46ff80f9a948cb423dc436eaec217487c7
Ashish Dhakal <ashishdhakal433@gmail.com>
2022-12-23 19:58:35 +0545

    add

---
+++ img/contact.php
@@ -0,0 +1,19 @@
+<?php
+ $name = $_POST['name'];
+ $sender_email= $_POST['email'];
+ $project =$_POST['project'];
+ $message =$_POST['message'];
+
+ $email_from ='laxmandnakal@gmail.com';
+$email_subject="New client message";
+
+$email_body="User Name:$name.\n";
+             "User Email:$visitor_email";
+               "User Message:$message.\n";
+
+    $to="ashishdhakal433@gmail.com";
+    $headers="From:$email_from\r\n:";
+    $headers="Reply-TO: $$visitor_email\r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+?>
